98eccd8ff8833ed356319773f6e61f4d43f8bf2a
Alexandros Mitakos <alexmitakos@gmail.com>
2017-02-06 05:39:38 +0200

    Add files via upload

--- vhdl/subkey_production.vhdl
+++ vhdl/subkey_production.vhdl
@@ -4,8 +4,8 @@ use ieee.std_logic_1164.all;
 
 entity subkey_production is
 generic(
-	shifting_parameter: in std_logic_vector(0 to 1));
-
+	shifting_parameter: in std_logic_vector(0 to 1);
+	left_or_right: in std_logic_vector(0 to 0)); --0 represents left shift while 1 right shift
 port(	left_key_in: in std_logic_vector(0 to 27);
 	right_key_in: in std_logic_vector(0 to 27);
 
@@ -27,6 +27,16 @@ architecture behavior of subkey_production is
 		data_out: out std_logic_vector(0 to 27));
 	end component;
 
+	component right_shift_by_1
+	port(	data_in: in std_logic_vector(0 to 27);
+		data_out: out std_logic_vector(0 to 27));
+	end component;
+
+	component right_shift_by_2
+	port(	data_in: in std_logic_vector(0 to 27);
+		data_out: out std_logic_vector(0 to 27));
+	end component;
+
 	component key_permutation_2 is
 	port(	left_half: in std_logic_vector(0 to 27);
 		right_half: in std_logic_vector(0 to 27);
@@ -41,29 +51,62 @@ begin
 
 	shift_by_1: if (shifting_parameter = "01") generate
 
-		s11: left_shift_by_1 port map(
-			data_in=> left_key_in,
-			data_out=> left_half_shifted);
+		left_shifting: if (left_or_right = "0") generate
+			s11: left_shift_by_1 port map(
+				data_in=> left_key_in,
+				data_out=> left_half_shifted);
 
-		s12: left_shift_by_1 port map(
-			data_in=> right_key_in,
-			data_out=> right_half_shifted);
+			s12: left_shift_by_1 port map(
+				data_in=> right_key_in,
+				data_out=> right_half_shifted);
+		end generate left_shifting;
+
+		right_shifting: if (left_or_right = "1") generate
+			s13: right_shift_by_1 port map(
+				data_in=> left_key_in,
+				data_out=> left_half_shifted);
+
+			s14: right_shift_by_1 port map(
+				data_in=> right_key_in,
+				data_out=> right_half_shifted);
+		end generate right_shifting;
+
 	end generate shift_by_1;
 
-	shift_by_2: if (shifting_parameter = "10") generate
-		s21: left_shift_by_2 port map(
-			data_in=> left_key_in,
-			data_out=> lefT_half_shifted);
 
-		s22: left_shift_by_2 port map(
-			data_in=> right_key_in,
-			data_out=> right_half_shifted);
+
+	shift_by_2: if (shifting_parameter = "10") generate
+
+		left_shifting: if (left_or_right = "0") generate
+			s21: left_shift_by_2 port map(
+				data_in=> left_key_in,
+				data_out=> lefT_half_shifted);
+
+			s22: left_shift_by_2 port map(
+				data_in=> right_key_in,
+				data_out=> right_half_shifted);
+		end generate left_shifting;
+
+		right_shifting: if (left_or_right = "1") generate
+			s23: right_shift_by_2 port map(
+				data_in=> left_key_in,
+				data_out=> left_half_shifted);
+
+			s24: right_shift_by_2 port map(
+				data_in=> right_key_in,
+				data_out=> right_half_shifted);
+		end generate right_shifting;
+
 	end generate shift_by_2;
+
+
 
 	error_case: if (shifting_parameter = "00" or shifting_parameter = "11") generate
 		--print error
 		assert false report "Error selecting left shift in subkey";
 	end generate error_case;
+
+
 
 	s3: key_permutation_2 port map(
 		left_half=> left_half_shifted,
